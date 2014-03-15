7b670c4aa626555eff5c46a3d1bbb0fd66bf062c
rb628 <rems.1989@gmail.com>
2014-03-15 16:21:07 -0400

    Added hash for context list

--- proj2/Dictionary.java
+++ proj2/Dictionary.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.regex.*;
 
@@ -11,54 +13,60 @@ import uk.ac.susx.informatics.Morpha;
 import weka.core.Stopwords;
 
 public class Dictionary {
-
-	static List<String> ContextWords = new ArrayList<String>();
+	static String id= "1";
+	static String Term = null;
+	public static Map<String, List<String>> ContextMap = new HashMap<String,List<String>>();
+	
 	static List<String> Context = new ArrayList<String>();
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\rems\\Documents\\NLP\\p2\\training_sample.data"));
-		//Retrieve the context words list surrounding the target word
-		ContextWords = PatternMatch(br);
-		//for(String word:ContextWords){
-		 //	   System.out.println(word);
-        //}
-        //Exclude stop words in the context list
-		Context = RemoveStopWords(ContextWords);
-		for(String word:Context){
-		  System.out.println(word);
-        }
-		//Lemmatize the words in the context list
-		Context = Lemmatize(Context);
-		for(String word:Context){
-			  System.out.println(word);
-	    }
-    }
-	
-	static List<String> PatternMatch(BufferedReader br) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\rems\\Documents\\NLP\\p2\\training_data.data"));
 		String sCurrentLine;
-		List<String> tokensVal = new ArrayList<String>();
-		List<String> context = new ArrayList<String>();
-		//Retrieve the example 
-		Pattern p = Pattern.compile("^(\\w+\\.\\w)\\s+\\|\\s+(\\d)\\s+\\|\\s+(.*)");
-		//Retrieve the context words surrounding the target word
-		Pattern pContext = Pattern.compile("(.*)\\%%(.*)\\%%(.*)");
-		Matcher m,mContext;
-				
+		
+		//Retrieve the context words list surrounding the target word
 		while ((sCurrentLine = br.readLine()) != null) {
+			List<String> tokensVal = new ArrayList<String>();
+			List<String> ContextWords = new ArrayList<String>();
+		    Pattern p = Pattern.compile("^(\\w+\\.\\w)\\s+\\|\\s+(\\d)\\s+\\|\\s+(.*)");
+			//Retrieve the context words surrounding the target word
+	     	Pattern pContext = Pattern.compile("(.*)\\%%(.*)\\%%(.*)");
+		   	Matcher m,mContext;
+						
 			m = p.matcher(sCurrentLine);
 			if (m.find()) {
 				tokensVal.add(m.group(3));
 			}			
-		    
-		}
-		for(String tokenContext:tokensVal){
-  		  mContext = pContext.matcher(tokenContext); 
-		  if(mContext.find()){
-			  context.add(mContext.group(1)+ mContext.group(3));
-		  }		
-		}
+		  
+			for(String tokenContext:tokensVal){
+	  		  mContext = pContext.matcher(tokenContext); 
+			  if(mContext.find()){
+				  Term = mContext.group(2);
+				  ContextWords.add(mContext.group(1)+ mContext.group(3));				  		  
+			  }		
+			}
 
-		return context;
-	}
+		 //Exclude stop words in the context list
+		 Context = RemoveStopWords(ContextWords);
+		 //for(String word:Context){
+		   //System.out.println(word);
+         //}
+		 //Lemmatize the words in the context list
+		 Context = Lemmatize(Context);
+	   	 //for(String word:Context){
+	 		//  System.out.println("Lemma"+word);
+	     //}
+	   	
+	   	 ContextMap.put(Term+id,Context);
+	   	 Integer ver = Integer.parseInt(id);
+	   	 ver = ver+1;
+	   	 id = ver.toString();
+	   	 
+		}//End while
+		
+		for (Map.Entry entry : ContextMap.entrySet()) {
+		    System.out.print("key,val: ");
+		    System.out.println(entry.getKey() + "," + entry.getValue());
+		}
+    }
 	
 	static List<String> RemoveStopWords(List<String> ContextWords){
 		String token;
@@ -93,7 +101,8 @@ public class Dictionary {
 			new MorphaStemmer();
 	 	    String morpha = MorphaStemmer.morpha(word,false);
 			ContextWords1.add(morpha);
-	 	}
+	 	    
+		}
 		return ContextWords1;
 		
 	}
